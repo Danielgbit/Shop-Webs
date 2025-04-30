9861feb466eb30358c88aeb158d7b854f0243c5b
daniel <danielbitmobb@gmail.com>
2025-04-30 18:47:12 -0500

    add folder header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return <>
+    <nav>
+  <ul>
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Que incluye</a></li>
+      <li><a href="#">Ejemplos</a></li>
+      <li><a href="#">Contacto</a></li>
+    </ul>
+    <button>Solicita la tuya!</button> 
+  </nav>
+  </>
+}
